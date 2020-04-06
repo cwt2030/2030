28d6bbe99024f81333be83b785f1b702593e5124
cwt2030 <cwt2030@gmail.com>
2020-04-06 17:19:07 +0300

    V9

--- login.php
+++ login.php
@@ -15,15 +15,7 @@
 
 <?php
 
-// فتح الجلسه لاستدعاء قيمتها
-session_start();
-
-// داله للتحقق من وجود قيمه فى جلسة username
-if(!isset($_SESSION['username'])){
-	// اذا كانت القيمه فارغه يتم الانتقال الى صفحة تسجيل الدخول
-	header("Location: login.php");
-	exit();
-}
+ 
 
 
 ?>
@@ -32,9 +24,9 @@ if(!isset($_SESSION['username'])){
 
 <?php
 // استدعاء ملف الاتصال بقاعدة البيانات
- 
+
 // فتح جلسه
-session_start();
+ 
 
 // دالة الشرط للتحقق من ضغط زر login
 if(isset($_POST['login'])){
@@ -47,15 +39,14 @@ if(isset($_POST['login'])){
 	$qu = "select * from userdata where Name = '$user' && Pass = '$pass'";
 	
 	// ارسال الاستعلام والتحقق من وجود العضو
-	if(mysqli_num_rows(mysqli_query($con, $qu)) > 0){
+	if(mysqli_num_rows(mysqli_query($conn, $qu)) > 0){
 		// اذا تم وجود النتيجة يتم اضافة اسم العضو فى الجلسه 
-		$_SESSION['username'] = $user;
-		// ثم يتم الانتقال الى منطقة الاعضاء
-		header("Location: cp.php");
-	} else {
+ 		// ثم يتم الانتقال الى منطقة الاعضاء
+		header("Location:typecode.html");}
+	else {
 		// اذا لم يتم ايجاد اى قيمه 0
 		echo 'اسم المستخدم او كلمة المرور خاطأ';
-	}
+	    }
 	
 	
 }
