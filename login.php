e7dd004cd61c112e83a49fe50fbf79a016dee099
cwt2030 <cwt2030@gmail.com>
2020-04-09 20:32:46 +0300

    V11

--- login.php
+++ login.php
@@ -10,44 +10,33 @@
  
      
  
-?>
 
 
-<?php
 
  
 
-
-?>
-
-
-
-<?php
-// استدعاء ملف الاتصال بقاعدة البيانات
-
-// فتح جلسه
  
-
-// دالة الشرط للتحقق من ضغط زر login
 if(isset($_POST['login'])){
 	// تخزين الحقول فى متغيرات
 	$user = $_POST['username'];
 	$pass = $_POST['password'];
-	
-	// انشاء استعلام
-	// فى هذا الاستعلام استخدمنا الشرط وجود اسم المستخدم وكلمة المرور
 	$qu = "select * from userdata where Name = '$user' && Pass = '$pass'";
+  
+
+ 
+
+if(mysqli_num_rows(mysqli_query($conn, $qu)) > 0){
+   header("Location:typecode.html");
 	
-	// ارسال الاستعلام والتحقق من وجود العضو
-	if(mysqli_num_rows(mysqli_query($conn, $qu)) > 0){
-		// اذا تم وجود النتيجة يتم اضافة اسم العضو فى الجلسه 
- 		// ثم يتم الانتقال الى منطقة الاعضاء
-		header("Location:typecode.html");}
-	else {
-		// اذا لم يتم ايجاد اى قيمه 0
-		echo 'اسم المستخدم او كلمة المرور خاطأ';
-	    }
+} 
+elseif ($user=='root' && $pass=='123' ) {
+   header("Location:admin.php");
+}
+else {
 	
-	
+	header("Location:index.html");
+ 
+ 
+ }
 }
 ?>
